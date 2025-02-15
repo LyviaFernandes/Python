f5eb3a308d37186f0277ccf2d4d2b0bb2a312722
LyviaFernandes <lyviafernandesborges@gmail.com>
2025-02-15 16:14:55 -0300

    Update and rename projetotatojpeg.html to idade.py

---
+++ idade.py
@@ -0,0 +1,7 @@
+print("Olá, humano! Bem vindo.")
+idade = int(input("Digite a sua idade: "))
+
+if idade >= 18 and idade <= 30:
+    print("Idade válida, pode entrar:)")
+else:
+    print("Idade inválida:((")
