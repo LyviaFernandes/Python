bfd0051dadfa017de7fb453699524e4309cdb4f6
LyviaFernandes <lyviafernandesborges@gmail.com>
2025-02-15 11:23:39 -0300

    Create modulo.py
    modulo

---
+++ modulo.py
@@ -0,0 +1,7 @@
+print("Olá humano! Como vai?")
+numero = int(input("Digite um número: "))
+
+if numero % 3 == 0 and numero % 5 == 0:
+    print("O número é multiplo de 3 e 5")
+else:
+    print("O número não é multiplo de 3 ou 5")
