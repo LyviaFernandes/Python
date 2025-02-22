e9994ca04dca38b15201ea84521db5ac1af0a012
LyviaFernandes <lyviafernandesborges@gmail.com>
2025-02-22 08:55:05 -0300

    Create outrowhile.py

---
+++ outrowhile.py
@@ -0,0 +1,12 @@
+# Exemplo uso de laço while
+
+print('Olá Humano')
+
+contador = 0 # inicializa a variavel
+
+while contador < 20:
+    if contador % 2 ==0:
+        print(contador)
+    contador += 1
+
+    print(\nTchau, Humano)
