e8adbc65f70b3825d208f98da2835818a3f912c9
LyviaFernandes <lyviafernandesborges@gmail.com>
2025-02-22 11:12:18 -0300

    Create tuplaordenadasorted.py
    ordenando tupla com sorted

---
+++ tuplaordenada.py
@@ -0,0 +1,8 @@
+#exemplo de como ordenar uma tupla
+
+candy = sorted(doces) #função quer ordenar a tupla doces
+
+print(f'A tupla original é {doces}')
+print(f'A tupla ordenada é {candy}')
+
+print('bye human')
