4dbe08e3acb436e77ba797f868cacbdc9481a1d5
LyviaFernandes <lyviafernandesborges@gmail.com>
2025-02-22 08:44:00 -0300

    Create laçocomwhile.py

---
+++ laçocomwhile.py
@@ -0,0 +1,11 @@
+# Exemplo de laço com while
+
+print('Olá, Humano!')
+
+controle = 1 # inicializando a variavel controle
+
+while controle < 10:
+    print(f'O valor da variavel agora é {controle}')
+    controle += 1
+
+print('Até a próxima, Humano<3')
