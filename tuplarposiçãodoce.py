27ebc83acb84ac8293312fb5d3fa654ff23522e3
LyviaFernandes <lyviafernandesborges@gmail.com>
2025-02-22 12:16:30 -0300

    Create tuplarposiçãodoce.py
    codigo com repetição e operação em tuplas

---
+++ tuplarposiçãodoce.py
@@ -0,0 +1,6 @@
+# Exemplo de codigo com repetição e operação em tuplas
+
+doces = ('sonho', 'chiclete', 'bis', 'chocolate', 'mufin', 'sorvete')
+for doce in doces:
+    cade = doces.index(doce)
+    print(f'\nO {doce} está na posição {cade} da tupla.')
