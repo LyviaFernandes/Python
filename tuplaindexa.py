0e3364e4ae5c9d07482e4b4ac0fcc0bd0bd885cb
LyviaFernandes <lyviafernandesborges@gmail.com>
2025-02-22 11:54:09 -0300

    Create tuplaindexa.py
    Exemplo de código de indexa um elemento da tupla

---
+++ tuplaindexa.py
@@ -0,0 +1,11 @@
+# Exemplo de código de indexa um elemento da tupla
+
+print('Olá humano')
+
+sobremesa = doces[4] #escolhendo o elemento de indice 4
+
+ondesta = doces.count(sobremesa)
+
+print(f'\nO elemento {sobremesa} de doces está na posição {ondesta} da tupla.')
+
+print('Falou humano')
