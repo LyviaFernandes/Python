d659ad058403f8531fdaacdeba08e8bea4d0d1f4
LyviaFernandes <lyviafernandesborges@gmail.com>
2025-03-08 11:37:36 -0300

    Create listaidademaismenos.py
    pede a idade e nome do usuario e exibe se o usuario é maior ou menor de idade e tambem a quantidade de maior e menor de idade

---
+++ listaidademaismenos.py
@@ -0,0 +1,21 @@
+nave = list() #lista principal
+aliens = list() #Nossa lista auxiliar
+escola = 0
+trabalho = 0
+
+for a in range(0,5):
+    aliens.append(str(input('Nome do alien:'))) #inserindo o nome
+    aliens.append(int(input('Idade do alien:'))) #inserindo a idade
+    nave.append(aliens[:]) #populando a lista principal
+    aliens.clear() # apagando a lista auxiliar
+    print('alien a bordo!!')
+
+    idade = nave[0][1]
+    nave.clear()
+    if idade < 18 :
+        escola += 1
+        print('este alien é um baby ainda')
+    elif idade >= 18:
+        trabalho += 1
+        print('Esse alien é um jovem senhor')
+print(f'temos {escola} babys e {trabalho} jovens senhores')
