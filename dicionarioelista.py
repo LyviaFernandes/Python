4e328c0830cb47578f8e7ca7591e2258e0464f23
LyviaFernandes <lyviafernandesborges@gmail.com>
2025-03-08 15:42:36 -0300

    Create dicionarioelista.py

---
+++ dicionarioelista.py
@@ -0,0 +1,11 @@
+filme1 = {'Titulo': 'Super Man','Diretor':'Richard Doner', 'ano': 1978}
+filme2 = {'Titulo': 'Matrix','Diretor':'Os Irmãos Wachowsky', 'ano': 1999}
+filme3 = {'Titulo': 'Star Wars','Diretor':'George Lucas', 'ano': 1977}
+
+locadora = [list]
+
+locadora.append(filme1)
+locadora.append(filme2)
+locadora.append(filme3)
+
+print(locadora)
