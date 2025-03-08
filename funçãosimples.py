83e582b008b43f05ad100ebc244874862d64621f
LyviaFernandes <lyviafernandesborges@gmail.com>
2025-03-08 16:13:22 -0300

    Create funçãosimples.py
    mensagem pré definida com def

---
+++ funçãosimples.py
@@ -0,0 +1,11 @@
+#Criando uma função simples
+
+#essa função exibe uma mensagem pré definida
+#essa é a definição da função 
+
+def iniciar():
+    print('              inicio            \n')
+#para chamar atenção, precisamos chamar ela, invocar a função
+# invocar a função é feito escrevendo o nome dela seguido de parenteses
+
+iniciar() # invocando função
