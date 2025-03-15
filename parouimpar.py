5b1f93d760fa78bdf95f5ad947888f8302e0bdb6
LyviaFernandes <lyviafernandesborges@gmail.com>
2025-03-15 09:21:05 -0300

    Create parouimpar.py

---
+++ parouimpar.py
@@ -0,0 +1,10 @@
+def par_ou_impar(y):
+    #ehpar(y)
+    #return 'esse numero é par'
+    if y % 2 == 0:
+        return(f'{y} é par')
+    #retun 'esse numero é impar'
+    else:
+        return(f'{y} é impar')
+    
+par_ou_impar(30)
